a6f9a3fed0a62180e4de0e54f2623fc93d2fa22a
annadeu3 <annadeu3@gmail.com>
2016-01-08 11:14:44 -0500

    add

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -1,9 +1,11 @@
 var fs = require('fs');
 
 var csvFile = fs.readFileSync("friend_list.csv", 'utf8');
-console.log(csvFile);
 
 function csvParse(csvFile) {
+	//each line in the csvFile is a contact-> separate each one
+	//do not include the first line (header)
+	//use slice method to remove the header -> .slice(1)
+	var contacts = csvFile.split("\n").slice(1);
 
 }
-console.log(csvParse(nameOfFile));
